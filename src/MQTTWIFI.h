f8a3b0296dfcc9fd47568c404d55e58237f2a55e
limitium <13qeadzc@WSX>
2020-06-03 00:14:02 +0300

    Added esp32 support

--- src/MQTTWIFI.h
+++ src/MQTTWIFI.h
@@ -1,4 +1,9 @@
+#if defined(ESP8266)
 #include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
+
 #include <PubSubClient.h>
 #include <Log4Esp.h>
 #include <cstring>
@@ -9,6 +14,7 @@ namespace MQTTWIFI
 #define SUB_CALLBACK_SIGNATURE std::function<void(const char *)>
 
 unsigned long MQTT_RECONNECT_TIMEOUT = 5000;
+const char *clientId = "MQTTWIFI" __DATE__ " " __TIME__;
 
 struct cmp_str
 {
@@ -24,15 +30,23 @@ public:
     MQTTWIFI() {}
     MQTTWIFI(const char *SSID, const char *password, const char *domain, uint16_t port)
     {
-        WiFiClient client;
-        MQTT_CALLBACK_SIGNATURE = std::bind(&MQTTWIFI::subscribeHandler, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
-        this->mqtt = PubSubClient(domain, port, callback, client);
         this->SSID = SSID;
         this->password = password;
+
+        client = new WiFiClient();
+        mqtt = new PubSubClient(*client);
+        mqtt->setServer(domain, port);
+        MQTT_CALLBACK_SIGNATURE = std::bind(&MQTTWIFI::subscribeHandler, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+        mqtt->setCallback(callback);
     }
     void init()
     {
+#if defined(ESP8266)
         WiFi.setSleepMode(WIFI_NONE_SLEEP);
+#else
+        WiFi.setSleep(false);
+#endif
+
         WiFi.begin(this->SSID, this->password);
         WiFi.setAutoConnect(true);
         WiFi.setAutoReconnect(true);
@@ -46,9 +60,9 @@ public:
     }
     void loop()
     {
-        unsigned long now = millis();
-        if (!mqtt.connected())
+        if (!mqtt->connected())
         {
+            unsigned long now = millis();
             if (now - lastReconnectMQTTAttempt > MQTT_RECONNECT_TIMEOUT)
             {
                 lastReconnectMQTTAttempt = now;
@@ -56,45 +70,52 @@ public:
                 {
                     lastReconnectMQTTAttempt = 0;
                 }
+                else
+                {
+                    log.verbose(F("Failed rc=%d"), mqtt->state());
+                }
             }
         }
         else
         {
-            mqtt.loop();
+            mqtt->loop();
         }
     }
     void publish(const char *topic, const char *payload)
     {
-        if (mqtt.connected())
+        if (mqtt->connected())
         {
             log.verbose(F(">> %s:%s"), topic, payload);
-            mqtt.publish(topic, payload);
+            mqtt->publish(topic, payload);
         }
     }
     void subscribe(const char *topic, SUB_CALLBACK_SIGNATURE callback)
     {
         log.verbose(F("sub:%s"), topic);
         callbacks[topic] = callback;
-        if (mqtt.connected())
+        if (mqtt->connected())
         {
-            mqtt.subscribe(topic);
+            log.verbose(F("real sub:%s"), topic);
+            mqtt->subscribe(topic);
         }
     }
     void unsubscribe(const char *topic)
     {
         log.verbose(F("unsub:%s"), topic);
         callbacks.erase(topic);
-        if (mqtt.connected())
+        if (mqtt->connected())
         {
-            mqtt.unsubscribe(topic);
+            mqtt->unsubscribe(topic);
         }
     }
 
 private:
     const char *SSID;
     const char *password;
+    WiFiClient *client;
+    PubSubClient *mqtt;
 
-    PubSubClient mqtt;
+    boolean con = false;
     unsigned long lastReconnectMQTTAttempt;
     std::map<const char *, SUB_CALLBACK_SIGNATURE, cmp_str> callbacks;
 
@@ -102,21 +123,22 @@ private:
 
     boolean reconnectMQTT()
     {
-        if (mqtt.connect("MQTTWIFI"))
+        if (mqtt->connect(clientId))
         {
-            log.verbose(F("Reconnected"));
+            log.verbose(F("Connected to MQTT"));
             for (const auto &pair : callbacks)
             {
-                mqtt.subscribe(pair.first);
+                mqtt->subscribe(pair.first);
             }
         }
-        return mqtt.connected();
+        return mqtt->connected();
     }
     void subscribeHandler(const char *topic, uint8_t *payload, unsigned int length)
     {
         payload[length] = '\0';
-        SUB_CALLBACK_SIGNATURE callback = callbacks[topic];
         log.verbose(F("<< %s:%s"), topic, payload);
+
+        SUB_CALLBACK_SIGNATURE callback = callbacks[topic];
         if (callback != nullptr)
         {
             return callback((const char *)payload);
